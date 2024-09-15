447e786ce6178741a90db51799d5fbadc0b1f686
William Fernando <wfernandofrancisco@gmail.com>
2024-09-15 20:08:53 -0300

    HEY-9: should be able to create a new question bigger than 255 characters

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +10,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\{User};
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+
+it('should be able to create a new question bigger than 255 characters', function () {
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act :: agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
+    ]);
+
+    // Assert :: verificar
+    $request->assertRedirect();
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+});
+
+it('should check if ends with question mark ?', function () {
+    expect(true)->toBeTrue();
+});
+
+it('should have at least 10 characters', function () {
+    expect(true)->toBeTrue();
+});
